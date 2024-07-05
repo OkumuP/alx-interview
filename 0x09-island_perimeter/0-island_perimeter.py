7652b9183669188c9622d317f7f53ef2d82729c8
OkumuP <okumupita@gmail.com>
2024-07-05 08:17:34 +0000

    Island is perimeter solution

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    '''
+    Calculates and returns the perimeter of an island
+    '''
+    width = len(grid[0])
+    height = len(grid)
+    perimeter = 0
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+    return perimeter
